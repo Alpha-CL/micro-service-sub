7b0de5903c2a894d3caf2b3c593fa6177690a641
Alphal-CL <xiaodao_92@yeah.net>
2023-07-08 19:20:08 +0800

    feat(mpa): test umi mpa config

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -1,3 +1,26 @@
 import { defineConfig } from '@umijs/max';
 
-export default defineConfig({});
+export default defineConfig({
+  mpa: {
+    template: 'templates/default.html',
+    getConfigFromEntryFile: true,
+    layout: '@/layouts',
+    entry: {
+      demo: {
+        description: 'Demo entry',
+      },
+    },
+  },
+  outputPath: 'build',
+  base: './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  manifest: {
+    basePath: './', // basePath: process.env.NODE_ENV === 'production' ? '/admin/' : '/',
+  },
+  routes: [
+    {
+      path: '/demo',
+      component: './Demo',
+    },
+  ],
+});
